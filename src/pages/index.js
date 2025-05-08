92fd68ddec65a04556825825fd3380c0cac2d406
Zeith <me@zeith.org>
2025-05-08 09:31:46 +0300

    Fix description

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="The central repository for all your VRChat Creator Companion needs!">
       <HomepageHeader />
       <main>
       </main>
